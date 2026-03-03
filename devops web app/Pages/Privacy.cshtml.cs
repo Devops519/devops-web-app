19fd68057cfc9dd0debd27a8cd7229dc2a57e9a0
Sudesh Kumar <Sudeshkumar1992june@gmail.com>
2026-03-03 17:07:19 +0530

    Add project files.

---
+++ devops web app/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace devops_web_app.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
